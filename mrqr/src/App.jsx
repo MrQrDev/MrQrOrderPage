c72ab490d7aca0543589bb7cd5f6c0945d5630b4
bishoe01 <bishoe01@gmail.com>
2023-12-10 01:03:19 +0900

    [feat] 에러페이지 제작 error-boundary 설정

--- mrqr/src/App.jsx
+++ mrqr/src/App.jsx
@@ -3,18 +3,30 @@ import { MenuContextProvider } from "./context/MenuListContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CookiesProvider } from "react-cookie";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
+import { Suspense } from "react";
+import { SyncLoader } from "react-spinners";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./pages/ErrorFallback";
 function App() {
   const queryClient = new QueryClient();
   return (
-    <CookiesProvider>
+    <Suspense fallback={<SyncLoader color="#36d7b7" />}>
       <QueryClientProvider client={queryClient}>
-        <MenuContextProvider>
-          <Outlet />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </MenuContextProvider>
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={() => {
+            queryClient.resetQueries();
+          }}
+        >
+          <CookiesProvider>
+            <MenuContextProvider>
+              <Outlet />
+              <ReactQueryDevtools initialIsOpen={false} />
+            </MenuContextProvider>
+          </CookiesProvider>
+        </ErrorBoundary>
       </QueryClientProvider>
-    </CookiesProvider>
+    </Suspense>
   );
 }
 
